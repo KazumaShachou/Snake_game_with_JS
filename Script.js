031b4796fb9e4e2bc81bb8f97b2b84383f28b6f5
KazumaShachou <raianimes1000@gmail.com>
2021-05-29 15:46:02 -0400

    add directions

--- Script.js
+++ Script.js
@@ -7,6 +7,8 @@ snake[0] = {
     y:8 * box
 }
 
+let direction = "right";
+
 function criarBG() {
     context.fillStyle = "violet";
     context.fillRect(0, 0, 16 * box, 16 * box);
@@ -19,5 +21,41 @@ function criarCobrinha(){
     }
 }
 
-criarBG(); 
-criarCobrinha();
+document.addEventListener('keydown', update);
+
+function update (event){
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
+function iniciarJogo(){
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0  && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].x > 15 * box && direction == "down") snake[0].x = 0;
+    if(snake[0].x < 0  && direction == "up") snake[0].x = 16 * box;
+    
+    criarBG(); 
+    criarCobrinha();
+
+    let snakeX = snake[0].x; 
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box; //direção da posição x quando a snake se mexer, acrescenta +1 quadrado 
+    if (direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+
+}
+
+let jogo = setInterval(iniciarJogo, 100); //100 milisegundos de intervalo, vai reiniciando e evita que ele trave
