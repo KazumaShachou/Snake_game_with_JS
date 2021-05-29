3ec8a0fc11458e44e54530974b670a449b295278
KazumaShachou <raianimes1000@gmail.com>
2021-05-29 17:52:41 -0400

    add more details

--- Script.js
+++ Script.js
@@ -8,6 +8,10 @@ snake[0] = {
 }
 
 let direction = "right";
+let food = {
+    x:Math.floor(Math.random() * 15 + 1) * box,
+    y:Math.floor(Math.random() * 15 + 1) * box,
+}
 
 function criarBG() {
     context.fillStyle = "violet";
@@ -21,6 +25,11 @@ function criarCobrinha(){
     }
 }
 
+function drawfood() {
+    context.fillStyle = "blue";
+    context.fillRect(food.x, food.y, box, box);
+}
+
 document.addEventListener('keydown', update);
 
 function update (event){
@@ -31,13 +40,21 @@ function update (event){
 }
 
 function iniciarJogo(){
-    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0  && direction == "left") snake[0].x = 16 * box;
     if(snake[0].x > 15 * box && direction == "down") snake[0].x = 0;
     if(snake[0].x < 0  && direction == "up") snake[0].x = 16 * box;
+
+    for(i = 1; i < snake.length; i++) {
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+             clearInterval(jogo);
+             alert("Game over! :(");
+        }
+    }
     
     criarBG(); 
     criarCobrinha();
+    drawfood();
 
     let snakeX = snake[0].x; 
     let snakeY = snake[0].y;
@@ -47,7 +64,13 @@ function iniciarJogo(){
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
-    snake.pop();
+    if(snakeX != food.x || snakeY != food.y) {
+        snake.pop();
+    }
+    else{
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
 
     let newHead = {
         x: snakeX,
